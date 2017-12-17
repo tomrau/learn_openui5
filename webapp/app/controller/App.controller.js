0d6c891c7e68ce169f937da1e756fe583d8b491d
Thomas Rauscher <tom_rauscher@live.at>
2017-12-17 21:46:21 +0100

    xml fragments

--- webapp/app/controller/App.controller.js
+++ webapp/app/controller/App.controller.js
@@ -1,35 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function(Controller, MessageToast, JSONModel, ResourceModel) {
-    "use strict";
-        return Controller.extend("sap.ui.demo.wt.controller.App", {
-            onInit: function() {
-                var oData = {
-                    recipient: {
-                        name: "World"
-                    }
-                };
-                var oModel = new JSONModel(oData);
-        
-                this.getView().setModel(oModel);
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
 
-                // set i18n model on view
-                var i18nModel = new ResourceModel({
-                    bundleName: "sap.ui.demo.wt.i18n.i18n"
-                });
-                this.getView().setModel(i18nModel, "i18n");
-            },
+	return Controller.extend("sap.ui.demo.wt.controller.App", {
+	});
 
-            onShowHello: function() {
-                // read msg from i18n model
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-                // show message
-                MessageToast.show(sMsg);
-            }
-        });
 });
